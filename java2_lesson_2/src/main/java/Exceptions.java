2ef248cf96dec53df7ce936c3689a97f6921115f
LevSavinkov <www.savinkov@gmail.com>
2021-04-27 19:26:50 +0400

    сделал 2 ДЗ

---
+++ java2_lesson_2/src/main/java/Exceptions.java
@@ -0,0 +1,22 @@
+public class Exceptions {
+
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        String[][] arr = new String[][]{
+                {"1", "1", "1", "3"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "1"},
+                {"1", "1", "1", "3"},
+                {"1", "1", "1", "3"}
+        };
+
+        sumArr.testArr(arr);
+
+    }
+
+
+
+}
+
+
+
+
